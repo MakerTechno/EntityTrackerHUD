c7bedb01dc9186c052e25523df5227fe22a35f44
MakerTechno <stevenyth@163.com>
2025-03-22 14:47:28 +0800

    Add collaborators' icons and configs. Optimized operation logic.

--- src/main/java/nowebsite/makertechno/entity_tracker/client/track/TrackerLogic.java
+++ src/main/java/nowebsite/makertechno/entity_tracker/client/track/TrackerLogic.java
@@ -1,6 +1,8 @@
 package nowebsite.makertechno.entity_tracker.client.track;
 
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.AABB;
@@ -8,59 +10,78 @@ import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.entity.PartEntity;
 import nowebsite.makertechno.entity_tracker.client.gui.TGui;
 import nowebsite.makertechno.entity_tracker.config.TConfig;
-import com.mojang.datafixers.util.Pair;
 import nowebsite.makertechno.entity_tracker.define.TCursor;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class TrackerLogic {
-    private static final List<Pair<Vec3, TCursor>> ENTITIES_POSITIONS = new ArrayList<>();
-    //private static int count = 0;
-    public static void addEntitiesPositions(List<Pair<Vec3, TCursor>> positions) {
+    private static final Set<Pair<? extends Entity, TCursor>> ENTITIES = new HashSet<>();
+    private static final Set<Pair<Vec3, TCursor>> ENTITIES_POSITIONS = new HashSet<>();
+    private static int count = 0;
+    public static void addEntities(Set<? extends Pair<? extends Entity, TCursor>> entities) {
+        synchronized (ENTITIES) {
+            ENTITIES.addAll(entities);
+        }
+    }
+    public static void clearEntities() {
+        synchronized (ENTITIES) {
+            ENTITIES.clear();
+        }
+    }
+    public static void setEntitiesPositions(Set<Pair<Vec3, TCursor>> positions) {
         synchronized (ENTITIES_POSITIONS) {
+            ENTITIES_POSITIONS.clear();
             ENTITIES_POSITIONS.addAll(positions);
         }
     }
-    public static void clearEntitiesPositions() {
-        synchronized (ENTITIES_POSITIONS) {
-            ENTITIES_POSITIONS.clear();
-        }
-    }
-    public static List<Pair<Vec3, TCursor>> getEntitiesPositions() {
+    public static Set<Pair<Vec3, TCursor>> getEntitiesPositions() {
         return ENTITIES_POSITIONS;
     }
 
     public static void trigger() {
-        /*if (count >=4) count = 0;
+        if (count >= 40) {
+            count = 0;
+            triggerEntities();
+        }
         else {
             count++;
-            return;
-        }*/
+            triggerPositions();
+        }
+    }
+    public static void triggerEntities() {
         Minecraft minecraft = Minecraft.getInstance();
         Player player = TGui.getCameraPlayer(minecraft);
         if (minecraft.level != null &&  player != null) {
-            clearEntitiesPositions();
+            clearEntities();
             for (Pair<EntityType<?>, TCursor> pair: TConfig.pointerWithEntities) {
-                List<Pair<Vec3, TCursor>> positions = minecraft.level.getEntities(
-                    pair.getFirst(),
-                    new AABB(
-                        player.getX() - 96.0D,
-                        player.getY() - 96.0D,
-                        player.getZ() - 96.0D,
-                        player.getX() + 96.0D,
-                        player.getY() + 96.0D,
-                        player.getZ() + 96.0D
-                    ),
-                    entity -> true
-                )
-                    .stream()
-                    .map(entity -> new Pair<>(entity, pair.getSecond()))
-                    .filter(pair1 -> !(pair1.getFirst() instanceof PartEntity))
-                    .map(pair1 -> new Pair<>(pair1.getFirst().position(), pair1.getSecond()))
-                    .toList();
-                addEntitiesPositions(positions);
+                addEntities(
+                    minecraft.level.getEntities(
+                        pair.getFirst(),
+                            new AABB(
+                                player.getX() - 64.0D,
+                                player.getY() - 64.0D,
+                                player.getZ() - 48.0D,
+                                player.getX() + 64.0D,
+                                player.getY() + 64.0D,
+                                player.getZ() + 64.0D
+                            ),
+                            entity -> true
+                        )
+                        .stream()
+                        .map(entity -> new Pair<>(entity, pair.getSecond()))
+                        .filter(pair1 -> !(pair1.getFirst() instanceof PartEntity))
+                        .collect(Collectors.toSet())
+                );
             }
         }
+    }
+    public static void triggerPositions() {
+        setEntitiesPositions(
+            ENTITIES.stream()
+                .map(pair -> new Pair<>(pair.getFirst().position(), pair.getSecond()))
+                .collect(Collectors.toSet())
+        );
     }
 }
--- src/main/java/nowebsite/makertechno/entity_tracker/define/TEntityIcon.java
+++ src/main/java/nowebsite/makertechno/entity_tracker/define/TEntityIcon.java
@@ -5,7 +5,12 @@ import net.minecraft.resources.ResourceLocation;
 public enum TEntityIcon implements TRenderItem {
     NONE("none","none", 1, 1),
     ENDER_DRAGON_HEAD("ender_dragon_head","ender_dragon.png", 16, 16),
-    WITHER_HEAD("wither_head","wither.png", 16, 16),;
+    WITHER_HEAD("wither_head","wither.png", 16, 16),
+    KING_SLIME("king_slime","king_slime.png", 16, 16),
+    EYE_OF_CTHULHU("eye_of_cthulhu","eye_of_cthulhu.png", 16, 16),
+    BRAIN_OF_CTHULHU("brain_of_cthulhu","brain_of_cthulhu.png", 16, 16),
+    QUEEN_BEE("queen_bee","queen_bee.png", 16, 16),
+    EATER_OF_WORLDS("eater_of_worlds","eater_of_worlds.png", 16, 16),;
     private final String name;
     private final ResourceLocation location;
     private final int width, height;
