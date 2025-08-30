c92a16afacca77968f1c5b120f87d87b6a2dee1c
MakerTechno <102357646+makertechno@users.noreply.github.com>
2025-08-30 18:26:28 +0800

    Let's rebuild

--- src/main/java/nowebsite/makertechno/entity_tracker/client/gui/TGui.java
+++ src/main/java/nowebsite/makertechno/entity_tracker/client/gui/TGui.java
@@ -5,8 +5,8 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.LayeredDraw;
 import net.minecraft.world.entity.player.Player;
-import nowebsite.makertechno.entity_tracker.config.TConfig;
-import nowebsite.makertechno.entity_tracker.client.track.TrackerLogic;
+import nowebsite.makertechno.entity_tracker.core.config.TConfig;
+import nowebsite.makertechno.entity_tracker.core.track.TrackerLogic;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
@@ -22,22 +22,10 @@ public class TGui implements LayeredDraw.Layer {
         return minecraft.getCameraEntity() instanceof Player player ? player : null;
     }
 
-    private void renderDir(GuiGraphics guiGraphics, Player player) {
-        TrackerLogic.getEntitiesPositions().forEach(
-            vec3CursorsPair -> {
-                switch (TConfig.mappingStyle) {
-                    case CENTER_RELATIVE -> RendererToScreen.centerRelativePointer(guiGraphics, player, vec3CursorsPair);
-                    case HEAD_FLAT -> {}
-                    case TRACK_FULL -> RendererToScreen.fullScreenPointer(guiGraphics, player, vec3CursorsPair, minecraft.gameRenderer);
-                }
-            }
-        );
-    }
-
     public void render(@NotNull GuiGraphics guiGraphics, @NotNull DeltaTracker deltaTracker) {
         Player player = getCameraPlayer(this.minecraft);
         if (!minecraft.options.hideGui && player != null && TConfig.available){
-            renderDir(guiGraphics, player);
+            TrackerLogic.getRENDERING().forEach((uuid, state) -> state.renderComponent(guiGraphics, deltaTracker.getGameTimeDeltaTicks(), player));
         }
     }
 }
