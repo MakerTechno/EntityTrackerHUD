ad9b20418748af98b49bb523b6eeb234dbf83fa9
westernat <westernat233@163.com>
2025-10-15 13:53:15 +0800

    移除EventBusSubscriber中的bus

--- src/main/java/nowebsite/makertechno/entity_tracker/core/event/TModClientGame.java
+++ src/main/java/nowebsite/makertechno/entity_tracker/core/event/TModClientGame.java
@@ -5,10 +5,10 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 import nowebsite.makertechno.entity_tracker.EntityTracker;
-import nowebsite.makertechno.entity_tracker.core.track.TrackerLogic;
 import nowebsite.makertechno.entity_tracker.core.config.TConfig;
+import nowebsite.makertechno.entity_tracker.core.track.TrackerLogic;
 
-@EventBusSubscriber(modid = EntityTracker.MOD_ID, bus = EventBusSubscriber.Bus.GAME, value = Dist.CLIENT)
+@EventBusSubscriber(modid = EntityTracker.MOD_ID, value = Dist.CLIENT)
 public class TModClientGame {
     @SubscribeEvent
     public static void onClientTick(ClientTickEvent.Pre event) {
